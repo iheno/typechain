e0bd879291baba9d9829f133a5d3e988aa386de7
Antonio-Jung <antonio_jung@wamazing.jp>
2019-11-12 17:47:33 +0900

    Creating a Block part Three

--- dist/index.js
+++ dist/index.js
@@ -11,10 +11,19 @@ class Block {
     }
 }
 Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
-const genesisBlock = new Block(0, "12313131", "", "hello", 123456);
+const genesisBlock = new Block(0, "2020202020202", "", "Hello", 123456);
 let blockchain = [genesisBlock];
-// console.log(blockchain);
 const getBlockchain = () => blockchain;
 const getLatestBlock = () => blockchain[blockchain.length - 1];
 const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previosBlock = getLatestBlock();
+    const newIndex = previosBlock.index + 1;
+    const newTimestamp = getNewTimeStamp();
+    const newHash = Block.calculateBlockHash(newIndex, previosBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previosBlock.hash, data, newTimestamp);
+    return newBlock;
+};
+console.log(createNewBlock("hello"));
+console.log(createNewBlock("bye bye"));
 //# sourceMappingURL=index.js.map
