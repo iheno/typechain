109ccf63864f829c509a0ef6c7f3881db006ad36
Antonio-Jung <antonio_jung@wamazing.jp>
2019-11-12 16:25:39 +0900

    Classes on Typescript

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,26 @@
 "use strict";
+// interface Human {
+//   name: string;
+//   age: number;
+//   gender: string;
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "heno",
-    age: 32,
-    gender: "male"
-};
+// for js add class
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+// const person = {
+//   name: "heno",
+//   age: 32,
+//   gender: "male"
+// };
+const Heno = new Human("Heno", 18, "male");
 const sayhi = (person) => {
     return `hello ${person.name}, you are ${person.age} olds, and ${person.gender}!`;
 };
-console.log(sayhi(person));
+console.log(sayhi(Heno));
 //# sourceMappingURL=index.js.map
